fd01a2f4c7c19a626cf93b46ba52dc6cdb841548
ajain727 <aj727@njit.edu>
2023-10-19 23:54:14 -0400

    Organized folders

---
+++ Project/Project2.js
@@ -0,0 +1,114 @@
+const receptionists = [
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        password: "JaneDoe@1967",
+        id: "9876",
+        email: "jdoe@gmail.com",
+    }
+];
+function toggleEmailRequiredText() {
+    const emailConfirmationCheckbox = document.getElementById("email_confirmation");
+    const emailRequiredText = document.querySelector("#email + .required-text");
+    if (emailConfirmationCheckbox.checked) {
+        emailRequiredText.style.display = "inline";
+    } else {
+        emailRequiredText.style.display = "none";
+    }
+}
+document.getElementById("email_confirmation").addEventListener("change", toggleEmailRequiredText);
+function validate(event) {
+    const fieldsToValidate = [
+        { id: "first_name", name: "First Name" },
+        { id: "last_name", name: "Last Name" },
+        { id: "password", name: "Password" },
+        { id: "receptionist_id", name: "Receptionist ID" },
+        { id: "phone", name: "Phone Number" },
+        { id: "email", name: "Email Address" },
+    ];
+    let errorFound = false;
+    for (const field of fieldsToValidate) {
+        const inputField = document.getElementById(field.id);
+
+        if (field.id === "password") {
+
+            if (
+                inputField.value.length > 16 ||
+                !/[A-Z]/.test(inputField.value) ||
+                !/[0-9]/.test(inputField.value) ||
+                !/[^A-Za-z0-9]/.test(inputField.value)
+            ) {
+                alert(`${field.name} is invalid. It should contain a maximum of 16 characters and have at least 1 uppercase letter, 1 numeric character, and 1 special character.`);
+                inputField.focus();
+                errorFound = true;
+                event.preventDefault();
+                return;
+            }
+        } else if (field.id === "receptionist_id") {
+
+            if (!/^\d{4}$/.test(inputField.value)) {
+                alert(`${field.name} is invalid. It should contain a 4-digit number.`);
+                inputField.focus();
+                errorFound = true;
+                event.preventDefault();
+                return;
+            }
+        } else if (field.id === "phone") {
+            if (!/^\d{10}$/.test(inputField.value.replace(/[-\s]/g, '')) || !/[-\s]/.test(inputField.value)) {
+                alert(`${field.name} is invalid. It should consist of 10 digits and may be delineated by spaces or dashes.`);
+                inputField.focus();
+                errorFound = true;
+                event.preventDefault();
+                return;
+            }
+        } else if (field.id === "email") {
+
+            const emailConfirmationCheckbox = document.getElementById("email_confirmation");
+            if (emailConfirmationCheckbox.checked) {
+                if (!/^\S+@\S+\.\S{3,5}$/.test(inputField.value)) {
+                    alert(`${field.name} is invalid. It must contain an @ followed by a period and an email domain with 3 to 5 characters.`);
+                    inputField.focus();
+                    errorFound = true;
+                    event.preventDefault();
+                    return;
+                }
+            }
+        } else {
+            if (inputField.value === "") {
+                alert(`${field.name} is missing. Please Enter.`);
+                inputField.focus();
+                errorFound = true;
+                event.preventDefault();
+                return;
+            }
+        }
+    }
+    if (!errorFound) {
+        verify()
+    }
+}
+function verify() {
+    const firstName = document.getElementById("first_name").value;
+    const lastName = document.getElementById("last_name").value;
+    const password = document.getElementById("password").value;
+    const receptionistId = document.getElementById("receptionist_id").value;
+    const email = document.getElementById("email").value;
+
+    for (const receptionist of receptionists) {
+        if (
+            firstName === receptionist.firstName &&
+            lastName === receptionist.lastName &&
+            password === receptionist.password &&
+            receptionistId === receptionist.id &&
+            (!email || email === receptionist.email)
+        ) {
+
+            alert(`Welcome, ${receptionist.firstName} ${receptionist.lastName}! You have entered the system to ${document.getElementById("transaction_type").value}.`);
+            return true;
+        }
+    }
+    alert("Account not found for" + " " + firstName + " " + lastName)
+
+    return false;
+}
+document.querySelector("form").addEventListener("submit", validate);
---
+++ Week6/week6_script.js
@@ -0,0 +1,11 @@
+function showHeading() {
+    var heading = document.getElementById("heading");
+    heading.style.visibility = "visible";
+    heading.style.opacity = 1;
+}
+
+function hideHeading() {
+    var heading = document.getElementById("heading");
+    heading.style.visibility = "hidden";
+    heading.style.opacity = 0;
+}
